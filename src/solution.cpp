ee4c4bede29e3918f1231f1eaf5770a90e033f3c
God Deam <martokonov@gmail.com>
2022-12-19 16:44:33 +0200

    added implementations to the tasks

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,12 +1,104 @@
 #include "solution.h"
 
+Trie::Trie() {
+
+    this->first = new TrieNode;
+
+}
+
+bool Trie::LetterExists(const char letter, TrieNode* curTrie) {
+
+    if (!curTrie->alphabet[letter - 97]) {
+        return false;
+    }
+    return true;
+}
+
+
 void Trie::insert(const char* word) {
-    return;
+    int index = 0;
+    TrieNode* cur = this->first;
+
+    while (true) {
+        
+        if (word[index + 1] == '\0') {
+
+            cur->alphabet[word[index] - 97] = new TrieNode;
+            cur = cur->alphabet[word[index] - 97];
+            cur->endOfWord = true;
+            break;
+        }
+
+        if (LetterExists(word[index], cur)) {
+            cur = cur->alphabet[word[index] - 97];
+            index++;
+        }
+        else {
+            cur->alphabet[word[index] - 97] = new TrieNode;
+            cur = cur->alphabet[word[index] - 97];
+            index++;
+        }
+
+    }
+
 };
+
 
 bool Trie::search(const char* word) {
+    int index = 0;
+    TrieNode* cur = this->first;
+
+    while (word[index] != '\0') {
+
+        if (LetterExists(word[index], cur)) {
+
+            cur = cur->alphabet[word[index] - 97];
+            index++;
+            continue;
+        }
+
+        return false;
+
+    }
+    if (cur->endOfWord) {
+        return true;
+    }
     return false;
+
 };
 
-Trie::~Trie() {
-}
+//void Trie::ClearAllNodes(TrieNode* node) {
+//
+//    if (isLeaf(node)) {
+//        node->endOfWord = false;
+//        delete node;
+//        return;
+//    }
+//
+//    for (size_t i = 0; i < 26; i++)
+//    {
+//        if (node->alphabet[i]) {
+//            ClearAllNodes(node->alphabet[i]);
+//        }
+//
+//    }
+//
+//}
+//
+//bool Trie::isLeaf(TrieNode* node) {
+//
+//    for (size_t i = 0; i < 26; i++)
+//    {
+//        if (node->alphabet[i]) {
+//            return false;
+//        }
+//    }
+//    return true;
+//}
+//
+//Trie::~Trie() {
+//
+//
+//    ClearAllNodes(this->first);
+//
+//}
--- src/solution.h
+++ src/solution.h
@@ -1,11 +1,31 @@
 #ifndef WEEK7_TREE_H
 
 class Trie {
+
+    struct TrieNode {
+        bool endOfWord;
+        TrieNode* alphabet[26];
+
+        TrieNode() {
+            endOfWord = false;
+            for (size_t i = 0; i < 26; i++)
+            {
+                alphabet[i] = nullptr;
+            }
+        }
+    };
+    
+    TrieNode* first;
+
 public:
+    Trie();
+ //   void ClearAllNodes(TrieNode* node);
+ //   bool isLeaf(TrieNode* node);
+    bool LetterExists(const char letter, TrieNode* curTrie);
     void insert(const char*);
     bool search(const char*);
     Trie& operator=(const Trie& other) = delete;
-    ~Trie();
+  //  ~Trie();
 };
 
 #define WEEK7_TREE_H
